2ff8e3d39de26d7b23ffb5c665912eb4335cfda4
GastonFrancisco <fgariel97@gmail.com>
2022-11-02 16:44:24 -0300

    Commit

--- force-app/main/default/lwc/allocateresource/allocateresource.js
+++ force-app/main/default/lwc/allocateresource/allocateresource.js
@@ -27,7 +27,7 @@ export default class Allocateresource extends LightningElement {
         }
     }
 
-    handleSelected(event){
+    handleSelected(event) {
         let realKey;
         let keyFakeArray = Object.keys(event.detail);
         keyFakeArray.forEach(key => realKey = key);
@@ -39,59 +39,46 @@ export default class Allocateresource extends LightningElement {
         console.log(!event.detail[realKey].includes(undefined));
         console.log(!event.detail[realKey].includes(null));
 
-        if(event.detail[realKey][0] == null && event.detail[realKey][1] == null){
+        if (event.detail[realKey][0] == null && event.detail[realKey][1] == null) {
             delete this.resourcesValues[realKey];
             console.log(JSON.parse(JSON.stringify(this.resourcesValues)))
         }
-        
-        if(this.disable == false && (event.detail[realKey].includes(undefined) || event.detail[realKey].includes(null))){
+
+        if (this.disable == false && (event.detail[realKey].includes(undefined) || event.detail[realKey].includes(null))) {
             this.disable = true
-        } else if(this.disable == true && (!event.detail[realKey].includes(undefined)) && (!event.detail[realKey].includes(null))){
+        } else if (this.disable == true && (!event.detail[realKey].includes(undefined)) && (!event.detail[realKey].includes(null))) {
             this.disable = false;
         }
-            
-        
-        // console.log(JSON.parse(JSON.stringify(this.resourcesValues)))
-        // if(this.disable == true){
-        //     let arrayKeys = Object.keys( this.resourcesValues);
-        //     console.log('Linea 39: ' + !event.detail[realKey].includes(undefined))
 
-        //     if((!event.detail[realKey].includes(undefined)) && arrayKeys.length >= 1){
-        //         console.log('Linea 42 ' + event.detail[realKey][0])
-        //         console.log('Linea 43: ' + event.detail[realKey].length == 2 && arrayKeys.length >= 1)
-        //         this.disable = false;
-        //     }
-        // } else if(event.detail[realKey].includes(null)){
-        //     this.disable = true
-        // }
-        // console.log('Linea 49 ' + event.detail[realKey][0])
+
+
 
     }
 
-    handleSubmit(){
-    //     metodoX({projectId: this.recordId, resourcesMap : JSON.stringify(this.submitAnswers)})
-    //     .then((status)=>{
-    //         if(status == 'Success'){
-    //             this.dispatchEvent(new ShowToastEvent({
-    //                 title: 'WOOHOO!',
-    //                 message: 'Youve got a shiny new badge',
-    //                 variant: 'success'
-    //             }));
-    //         }else{
-    //             this.dispatchEvent(new ShowToastEvent({
-    //                 title: 'Try again',
-    //                 message: 'Youve got one or more wrong answers',
-    //                 variant: 'error'
-    //             }));
-    //         }
-    //     })
-    //     .catch((error) =>{
-    //         console.log(error);
-    //         this.dispatchEvent(new ShowToastEvent({
-    //             title: 'Error',
-    //             message: error.message,
-    //             variant: 'error'
-    //         }));
-    //     })  
+    handleSubmit() {
+        metodoX({ projectId: this.recordId, resourcesMap: JSON.stringify(this.submitAnswers) })
+            .then((status) => {
+                if (status == 'Success') {
+                    this.dispatchEvent(new ShowToastEvent({
+                        title: 'WOOHOO!',
+                        message: 'Youve got a shiny new badge',
+                        variant: 'success'
+                    }));
+                } else {
+                    this.dispatchEvent(new ShowToastEvent({
+                        title: 'Try again',
+                        message: 'Youve got one or more wrong answers',
+                        variant: 'error'
+                    }));
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Error',
+                    message: error.message,
+                    variant: 'error'
+                }));
+            })
     }
 }
